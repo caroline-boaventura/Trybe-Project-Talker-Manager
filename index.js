b7eb81b0270ca0db1907c3f89edf6918d3cb10d6
Caroline Boaventura <caroline.boaventura08@gmail.com>
2021-12-09 18:24:02 -0300

    Resolve requisito 1 e 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,39 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+// Função que retorna Json lido =====
+function readFile() {
+  try {
+    const data = fs.readFileSync('./talker.json');
+    return data;
+  } catch (err) {
+    return [];
+  }
+}
+
+// Requisito 1 =====
+app.get('/talker', (req, res) => {
+  const data = readFile();
+  const talkers = JSON.parse(data);
+
+  res.status(200).json(talkers);
+});
+
+// Requisito 2 =====
+app.get('/talker/:id', (req, res) => {
+  const data = readFile();
+  const talkers = JSON.parse(data);
+
+  const { id } = req.params;
+  const talkerFind = talkers
+    .find((talker) => talker.id === parseInt(id, 10));
+  
+    if (!talkerFind) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    res.status(200).json(talkerFind);
+});
+
+// Requisito 3 =====
 
 app.listen(PORT, () => {
   console.log('Online');
