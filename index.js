d4f4a9ac7dec11d0f6f79d4a499492905bb1d403
Caroline Boaventura <caroline.boaventura08@gmail.com>
2021-12-10 18:18:28 -0300

    Resolve requisito 4

--- index.js
+++ index.js
@@ -66,8 +66,8 @@ function tokenGenerator() {
 
 // regex retirado de https://www.horadecodar.com.br/2020/09/13/como-validar-email-com-javascript/
 function validate(email) {
-  const re = /\S+@\S+\.\S+/;
-  return re.test(email); // retorna true ou false
+  const regex = /\S+@\S+\.\S+/;
+  return regex.test(email); // retorna true ou false
 }
 
 const validateEmailMiddlewar = (req, res, next) => {
@@ -114,6 +114,113 @@ app.post(
 );
 
 // Requisito 4 =======
+const validateTokenMiddleware = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+
+  if (authorization.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+
+  next();
+};
+
+const validateNameMiddleware = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const validateAgeMiddleware = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+const validateTalkMiddleware = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (!talk || talk === '') {
+    return res.status(400).json({ 
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+
+  next();
+};
+
+const validateWatchedAtMiddleware = (req, res, next) => {
+  const { watchedAt } = req.body.talk;
+
+  const dateRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
+  // Regex retirado de https://www.regextester.com/99555
+  const testRegex = dateRegex.test(watchedAt);
+
+  if (!watchedAt || watchedAt === '') {
+    return res.status(400).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+
+  if (!testRegex) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  next();
+};
+
+const validateRateMiddleware = (req, res, next) => {
+  const { rate } = req.body.talk;
+
+  if (!rate || rate === '') {
+    return res.status(400).json({ 
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+
+  if (!(rate <= 5 && rate >= 1)) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  next();
+};
+
+app.post(
+  '/talker',
+  validateTokenMiddleware,
+  validateNameMiddleware,
+  validateAgeMiddleware,
+  validateTalkMiddleware,
+  validateWatchedAtMiddleware,
+  validateRateMiddleware,
+  async (req, res) => {
+    const { name, age, talk: { watchedAt, rate } } = req.body;
+    const data = await readFile();
+    const talkers = JSON.parse(data);
+    const id = talkers.length + 1;
+    const objNewTalker = { id, name, age, talk: { watchedAt, rate } };
+    talkers.push(objNewTalker);
+    await fs.writeFileSync('./talker.json', JSON.stringify(talkers));
+    
+    res.status(201).json(objNewTalker);
+},
+);
 
 app.listen(PORT, () => {
   console.log('Online');
